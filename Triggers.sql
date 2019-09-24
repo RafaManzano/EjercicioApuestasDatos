73f6ac05db2129e63916d23cbf93e14cfbcd7c17
RafaManzano <rafamanzanomedina@gmail.com>
2019-09-24 11:03:24 +0200

    Procedimiento para comprobar apuestas
    Con este procedimiento se comprueba si una apuesta es acertada o no, devolviendo un bit  (boolean)

--- Triggers.sql
+++ Triggers.sql
@@ -1,5 +1,5 @@
 USE ApuestasDeportivas
-
+--USE master
 --En la tabla ingresos cuando se hace un insert hay que hacer un trigger que aumente el saldo del usuario 
 --(Cuando se retira también funcionaria igual).
 GO
@@ -37,3 +37,43 @@ AFTER INSERT AS
 			ROLLBACK
 		END
 	END
+
+
+--Procedimiento que comprueba que una apuesta es ganada o no
+GO
+CREATE PROCEDURE comprobarApuestaAcertada @idApuesta INT, @tipo INT
+AS
+BEGIN
+DECLARE @acertada BIT
+	IF(@tipo = 1)
+	BEGIN
+		IF EXISTS (SELECT * FROM Apuestas AS A
+		INNER JOIN Partidos AS P ON A.id_partido = P.id AND A.id = @idApuesta
+		WHERE A.golLocal = P.golLocal AND A.golVisitante = P.golVisitante)
+		BEGIN
+			SET @acertada = 1
+		END
+	END
+
+	IF(@tipo = 2)
+	BEGIN
+		IF EXISTS (SELECT * FROM Apuestas AS A
+		INNER JOIN Partidos AS P ON A.id_partido = P.id AND A.id = @idApuesta
+		WHERE A.puja = '1' AND A.golLocal = P.golVisitante OR A.puja = '2' AND A.golVisitante = P.golVisitante)
+		BEGIN
+			SET @acertada = 1
+		END
+	END
+
+	IF(@tipo = 3)
+	BEGIN
+		IF EXISTS (SELECT * FROM Apuestas AS A
+		INNER JOIN Partidos AS P ON A.id_partido = P.id AND A.id = @idApuesta
+		WHERE A.puja = '1' AND P.golLocal > P.golVisitante OR A.puja = '2' AND P.golLocal < P.golVisitante OR A.puja = 'x' AND P.golLocal = P.golVisitante)
+		BEGIN
+			SET @acertada = 1
+		END
+	END
+	RETURN @acertada
+END
+GO
