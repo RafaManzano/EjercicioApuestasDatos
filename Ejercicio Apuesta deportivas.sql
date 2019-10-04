f8300d2dcdda4b18f11e55f155fa047b7d492fc8
vperezlobato <55490747+vperezlobato@users.noreply.github.com>
2019-10-04 09:37:04 +0200

    cambio

--- Ejercicio Apuesta deportivas.sql
+++ Ejercicio Apuesta deportivas.sql
@@ -75,6 +75,10 @@ CREATE TABLE Apuestas_tipo2(
 	apuestasMáximas money not null
 		constraint ck_Apuesta_tipo2_apuestasMáximas check (apuestasMáximas > 0),
 	fechaHora datetime not null,
+	gol tinyint not null
+		constraint ck_Apuestas_tipo2_gol check(gol >= 0),
+	puja char(1) not null
+		constraint ck_Apuestas_tipo2_puja check(puja in ('1','2','3')),
 	constraint fk_Apuestas_tipo2 foreign key (id) references Apuestas(id)
 )
 
@@ -83,5 +87,7 @@ CREATE TABLE Apuestas_tipo3(
 	--------------------------------------------------------------------------
 	apuestasMáximas money not null
 		constraint ck_partidos_apuestasMáximas check (apuestasMáximas > 0),
+	puja char(1) not null
+		constraint ck_Apuestas_tipo3_puja check(puja in ('1','2','3'))
 	constraint fk_Apuestas_tipo3 foreign key (id) references Apuestas(id)
 )
