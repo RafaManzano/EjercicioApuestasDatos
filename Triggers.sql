b6b4e285dbfbe6f7ed98aad932d28c70acccace6
RafaManzano <rafamanzanomedina@gmail.com>
2019-10-04 09:59:31 +0200

    Subiendo triggers

--- Triggers.sql
+++ Triggers.sql
@@ -116,3 +116,37 @@ AFTER UPDATE, INSERT AS
 		END
 	END
 GO
+
+--1er Trigger actualiza el saldo del usuario cuando realiza una apuesta
+GO	
+CREATE OR ALTER TRIGGER actualizarSaldo on Apuestas
+AFTER INSERT AS
+	BEGIN
+		DECLARE @saldo money
+		DECLARE @cantidad int
+		DECLARE @id_usuario smallint 
+		SELECT @saldo = U.saldo, @cantidad=I.cantidad, @id_usuario=U.id FROM Usuarios AS U 
+		INNER JOIN inserted AS I ON U.id = I.id_usuario
+
+		
+		IF(@cantidad > @saldo)
+			BEGIN
+				RAISERROR('No tiene suficiente saldo',16,1)
+				ROLLBACK
+			END
+		ELSE
+			BEGIN
+				UPDATE Usuarios
+				SET saldo -= @cantidad WHERE id=@id_usuario
+				--Insertamos el ingreso
+				INSERT INTO Ingresos (cantidad, descripcion, id_usuario) VALUES (@cantidad,'Apuesta realizada',@id_usuario)
+			END
+	END
+GO
+
+/*
+	Comprobar los beneficios maximos para no dejar pagar la apuesta
+*/
+
+-- Se comprueba que en cada apuesta ganada no se supere el maximo beneficio definido en la tabla
+-- Si esto ocurre, el pago de la apuesta quedaria anulada
